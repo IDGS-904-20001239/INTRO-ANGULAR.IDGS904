875e5d4cad0580c2bc76670562d991f95fb9ea22
yair <yair.teran47@gmail.com>
2023-06-13 22:56:41 -0600

    EYET 13/06/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { escuelaComponent } from './escuela/escuela.component';
+import { ievnComponent } from './IEVN/ievn.component';
+import { IricComponent } from './escuela/iric/iric.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    escuelaComponent,
+    ievnComponent,
+    IricComponent
   ],
   imports: [
     BrowserModule
